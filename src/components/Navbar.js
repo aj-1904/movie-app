deccdbe14075d0f355d89a35d1d1c7d5217f7ccd
aj-1904 <jainayush827@gmail.com>
2021-06-17 22:58:29 +0530

    Setting UI and created components

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { data } from '../data'
+
+class Navbar extends React.Component {
+
+    render() {
+        return (
+            <div className="nav">
+              <div className="search-container">
+                <input placeholder="Search a Movie..."/>
+                <button id="search-btn">Search</button>
+              </div>
+            </div>
+          );
+    }
+  
+}
+
+export default Navbar;
