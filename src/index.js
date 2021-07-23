4dcb0338573a1b7d883578324c722e5a00a96e00
aj-1904 <jainayush827@gmail.com>
2021-07-23 14:06:11 +0530

    Middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import './index.css';
-import App from './components/App';
-import rootReducer from './reducers'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import "./index.css";
+import App from "./components/App";
+import rootReducer from "./reducers";
+
+// Curried form - function logger (obj, next, action)
+// logger(obj)(next)(action)
+const logger = function ({ dispatch, getState }) {
+  return function (next) {
+    return function (action) {
+      // middleware code
+      console.log("ACTION_TYPE = ", action.type);
+      next(action);
+    };
+  };
+};
 
 //Create Store
-const store = createStore(rootReducer);
-console.log('store', store);
+const store = createStore(rootReducer, applyMiddleware(logger));
+console.log("store", store);
 // console.log('BEFORE STATE', store.getState());
 
 // //dispatch used to send actions
@@ -19,9 +31,7 @@ console.log('store', store);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App store={store}/>
+    <App store={store} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
